023c99d0c5b85702f332c59cb3d7f54f258fb032
ihab <ihab_alnaqib@yahoo.com>
2018-05-07 11:22:06 +0300

    Add files via upload

---
+++ src/main/java/com/expedia/test/constants/AppConstants.java
@@ -0,0 +1,6 @@
+package com.expedia.test.constants;
+
+public class AppConstants {
+
+	public static final String URL = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel";
+}
