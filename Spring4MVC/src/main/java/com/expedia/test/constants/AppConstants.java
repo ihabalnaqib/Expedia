3353f4e601828944739542a8d95e0073afbb4d7a
TAGO\inaqib <ihab_alnaqib@yahoo.com>
2018-05-07 12:58:12 +0300

    final commit

--- Spring4MVC/src/main/java/com/expedia/test/constants/AppConstants.java
+++
@@ -1,6 +0,0 @@
-package com.expedia.test.constants;
-
-public class AppConstants {
-
-	public static final String URL = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel";
-}
